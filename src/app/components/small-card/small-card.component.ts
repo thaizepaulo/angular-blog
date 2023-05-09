12f853bd68f9251d123cbe86488590805e84cadf
thaizepaulo <thaizepaulo@gmail.com>
2023-05-09 17:47:37 -0300

    conteúdo criado

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SmallCardComponent implements OnInit {
 
   @Input()
+  Id:string = "0";
+  @Input()
   photoCover:string = "";
   @Input()
   cardTitle:string = "";
